29c3726fe83fc1aaec106f6fb10b396604d67c72
Aidan Burrowes <84931497+FluffyNova@users.noreply.github.com>
2023-03-26 10:12:52 -0400

    description

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,3 +1,48 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:78bda37c15bd47a6aaaff5b95113dcbcb7a21de2fe00fc0c2e6f6aad060487ea
-size 1914
+import React, { useState } from 'react';
+import "./home.css";
+import Quiz from './quiz';
+import CreateAccount from './CreateAccount';
+import "./testlogin.js"
+import "./createuser.js"
+
+function Home() {
+
+const [showQuiz, setShowQuiz] = useState(false);
+const [createNewAccount, setCreateNewAccount] = useState(false);
+const [login, setLogin] = useState(false);
+
+const handleStartButtonClick = () => {
+    login(true)
+    setShowQuiz(true);
+};
+
+const handleSignInButtonClick = () => {
+    createNewAccount(true);
+    setShowQuiz(true);
+}
+
+  return (
+    <content>
+      {showQuiz ? (
+                
+                <Quiz />
+              ) : (
+                <div className='bruh'>
+                <h1>Welcome to ChowNow.</h1>
+                <p className='about'>Introducing ChowNow, a web application designed to make dining out easier and more enjoyable. ChowNow offers users the ability to select their favorite foods and preferences in order to receive personalized restaurant recommendations. The user can also create an account to save their preferences to get recommendations instantaneously.</p>
+                <div className='actions'>
+                  <button className="home" onClick={() => handleStartButtonClick()}>Get Started</button>
+                  <p>or</p>
+                  <button className='home' onClick={() => handleSignInButtonClick()}>Sign In</button>
+                  <p>if you have an account</p>
+                </div>
+                
+              </div>
+              )
+              }
+    </content>
+    
+  );
+}
+
+export default Home;
